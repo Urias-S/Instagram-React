6cc94557e5ff14ab8b8d57372122c8c0984cd32d
Vinicius <viniciusmoura761@gmail.com>
2025-02-19 22:17:06 -0300

    feat: add the navbar script

---
+++ src/Navbar.js
@@ -0,0 +1,49 @@
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <Logo />
+        <Pesquisa />
+        <Icones />
+        <Icones_mobile />
+      </div>
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <ion-icon name="logo-instagram"></ion-icon>
+      <div class="separador"></div>
+      <img src="assets/img/logo.png" alt="logo" />
+    </div>
+  );
+}
+
+function Pesquisa() {
+  return (
+    <div className="pesquisa">
+      <input type="text" placeholder="Pesquisar" />
+    </div>
+  );
+}
+
+function Icones() {
+  return (
+    <div className="icones">
+      <ion-icon name="paper-plane-outline"></ion-icon>
+      <ion-icon name="compass-outline"></ion-icon>
+      <ion-icon name="heart-outline"></ion-icon>
+      <ion-icon name="person-outline"></ion-icon>
+    </div>
+  );
+}
+
+function Icones_mobile () {
+  return (
+    <div className="icones-mobile">
+      <ion-icon name="paper-plane-outline"></ion-icon>
+    </div>
+  );
+}
