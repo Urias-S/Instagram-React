1276615b61b204908336f12c13e46912c8f4bd88
Vinicius <viniciusmoura761@gmail.com>
2025-02-23 18:33:41 -0300

    feat: added change username function

--- src/User.js
+++ src/User.js
@@ -1,11 +1,21 @@
+import React from "react";
 export default function User() {
+  const [name, setUsername] = React.useState('catanacomics')
+  const changeUsername = () => {
+    let newName = prompt('Qual é o seu nome?');
+    if (newName) {
+      setUsername(newName);
+    } else {
+      return;
+    }
+}
   return (
     <div className="usuario">
       <img src="assets/img/catanacomics.svg" />
       <div className="texto">
         <span>
-          <strong>catanacomics</strong>
-          <ion-icon name="pencil"></ion-icon>
+          <strong>{name}</strong>
+          <ion-icon name="pencil" onClick = {changeUsername}></ion-icon>
         </span>
       </div>
     </div>
