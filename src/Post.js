79fdea772f6a31cd3b0cdbd9d771345ab9a11814
Vinicius <viniciusmoura761@gmail.com>
2025-02-26 19:25:40 -0300

    feat: feed posts via props

--- src/Post.js
+++ src/Post.js
@@ -1,89 +1,65 @@
 import React from "react";
-export default function Post() {
-  const posts = [
-    {
-      userImg: 'assets/img/meowed.svg',
-      userName: 'meowed',
-      imgPost: 'assets/img/gato-telefone.svg',
-      likedImg: 'assets/img/respondeai.svg',
-      likedUser: 'respondeai',
-      likes: 101523,
-    },
-    {
-      userImg: 'assets/img/barked.svg',
-      userName: 'barked',
-      imgPost: 'assets/img/dog.svg',
-      likedImg: 'assets/img/adorable_animals.svg',
-      likedUser: 'adorable_animals',
-      likes: 99159,
-    },
-    {
-      userImg: 'assets/img/meowed.svg',
-      userName: 'meowed',
-      imgPost: 'assets/img/gato-telefone.svg',
-      likedImg: 'assets/img/respondeai.svg',
-      likedUser: 'respondeai',
-      likes: 100236,
-    }
-  ];
 
-  const [pinned, setPinned] = React.useState(posts.map(() => false));
+export default function Post({
+  userImg,
+  userName,
+  imgPost,
+  likedImg,
+  likedUser,
+  likes
+}) {
+  const [pinned, setPinned] = React.useState(false);
+  const [liked, setLiked] = React.useState(false);
 
-  const pinButton = (index) => {
-    const pinnedCompiled = pinned.slice();
-    pinnedCompiled[index] = !pinnedCompiled[index];
-    setPinned(pinnedCompiled);
-  }
+  const pinButton = () => {
+    setPinned(!pinned);
+  };
 
-  const [liked, setLiked] = React.useState(posts.map(() => false));
+  const likePostToggle = () => {
+    setLiked(!liked);
+  };
 
-  const likePostToggle = (index) => {
-    const likedCompiled = liked.slice();
-    likedCompiled[index] = !likedCompiled[index];
-    setLiked(likedCompiled);
-  }
+  const likePostAdd = () => {
+    setLiked(true);
+  };
 
-  const likePostAdd = (index) => {
-    const likedCompiled = liked.slice();
-    likedCompiled[index] = true;
-    setLiked(likedCompiled);
-  }
-
-  const compiledPosts = posts.map((post, index) => {
-    return (
-      <div className="post" key={index}>
-        <div className="topo">
-          <div className="usuario">
-            <img src={post.userImg} />
-            {post.userName}
+  return (
+    <div className="post">
+      <div className="topo">
+        <div className="usuario">
+          <img src={userImg} alt={userName} />
+          {userName}
+        </div>
+        <div className="acoes">
+          <ion-icon name="ellipsis-horizontal"></ion-icon>
+        </div>
+      </div>
+      <div className="conteudo">
+        <img onClick={likePostAdd} src={imgPost} alt="Post content" />
+      </div>
+      <div className="fundo">
+        <div className="acoes">
+          <div>
+            <ion-icon
+              style={liked ? { color: 'red' } : { color: 'black' }}
+              onClick={likePostToggle}
+              name={liked ? 'heart' : 'heart-outline'}
+            ></ion-icon>
+            <ion-icon name="chatbubble-outline"></ion-icon>
+            <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
-          <div className="acoes">
-            <ion-icon name="ellipsis-horizontal"></ion-icon>
+          <div>
+            <ion-icon onClick={pinButton} name={pinned ? 'bookmark' : 'bookmark-outline'}></ion-icon>
           </div>
         </div>
-        <div className="conteudo">
-          <img onClick = {() => likePostAdd(index)}src={post.imgPost} />
-        </div>
-        <div className="fundo">
-          <div className="acoes">
-            <div>
-              <ion-icon style ={liked[index] ? {color: 'red'} : {color: 'black'}} onClick = {() => likePostToggle(index)} name= {liked[index] ? 'heart' : 'heart-outline'}></ion-icon>
-              <ion-icon name="chatbubble-outline"></ion-icon>
-              <ion-icon name="paper-plane-outline"></ion-icon>
-            </div>
-            <div>
-              <ion-icon onClick = {() => pinButton(index)} name={pinned[index] ? 'bookmark' : 'bookmark-outline'}></ion-icon>
-            </div>
-          </div>
 
-          <div className="curtidas">
-            <img src={post.likedImg} />
-            <div className="texto">
-              Curtido por <strong>{post.likedUser}</strong> e <strong>outras {liked[index] ? (post.likes +1) : post.likes} pessoas</strong>
-            </div>
+        <div className="curtidas">
+          <img src={likedImg} alt={likedUser} />
+          <div className="texto">
+            Curtido por <strong>{likedUser}</strong> e <strong>outras {liked ? likes + 1 : likes} pessoas</strong>
           </div>
         </div>
-      </div>)
-  });
-  return compiledPosts;
+      </div>
+    </div>
+  );
 }
--- src/Posts.js
+++ src/Posts.js
@@ -1,8 +1,46 @@
 import Post from "./Post";
-export default function Posts () {
+
+const posts = [
+  {
+    userImg: 'assets/img/meowed.svg',
+    userName: 'meowed',
+    imgPost: 'assets/img/gato-telefone.svg',
+    likedImg: 'assets/img/respondeai.svg',
+    likedUser: 'respondeai',
+    likes: 101523,
+  },
+  {
+    userImg: 'assets/img/barked.svg',
+    userName: 'barked',
+    imgPost: 'assets/img/dog.svg',
+    likedImg: 'assets/img/adorable_animals.svg',
+    likedUser: 'adorable_animals',
+    likes: 99159,
+  },
+  {
+    userImg: 'assets/img/meowed.svg',
+    userName: 'meowed',
+    imgPost: 'assets/img/gato-telefone.svg',
+    likedImg: 'assets/img/respondeai.svg',
+    likedUser: 'respondeai',
+    likes: 100236,
+  }
+];
+
+export default function Posts() {
   return (
     <div className="posts">
-      <Post />
+      {posts.map((post, index) => (
+        <Post
+          key={index}
+          userImg={post.userImg}
+          userName={post.userName}
+          imgPost={post.imgPost}
+          likedImg={post.likedImg}
+          likedUser={post.likedUser}
+          likes={post.likes}
+        />
+      ))}
     </div>
   );
 }
